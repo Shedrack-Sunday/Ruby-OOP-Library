d119fa12f4588a581bf0864979ed95f21e68e05a
solly <solobarine@gmail.com>
2023-01-06 01:28:08 +0100

    Create successful test for nameable

--- nameable_rspec.rb
+++ nameable_rspec.rb
@@ -3,8 +3,8 @@ require './nameable.rb'
 describe Nameable do
   describe '#correct_name' do
     it 'Returns Not Implemented Error' do
-      name = Nameable.correct_name
-      expect(name).to be_a(String)
+      name = Nameable.new
+      expect { name.correct_name }.to raise_error(NotImplementedError)
     end
   end
 end
