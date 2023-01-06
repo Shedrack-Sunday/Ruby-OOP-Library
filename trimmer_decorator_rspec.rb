e5af2355a3bbe44086491677450615562a6c51a2
solly <solobarine@gmail.com>
2023-01-06 01:14:04 +0100

    write test for trimmer_decorator

--- trimmer_decorator_rspec.rb
+++ trimmer_decorator_rspec.rb
@@ -1,11 +1,13 @@
+require './decorator.rb'
 require './trimmer_decorator.rb'
 
 describe TrimmerDecorator do
   describe '#correct_name' do
     it 'Trims white spaces from given string' do
-      input = TrimmerDecorator.new('  tryouts for this')
-      result = input.correct_name
-      expect(result).to be('  tryouts')
+      nameable = double('nameable')
+      allow(nameable).to receive(:correct_name).and_return('Peter Pan')
+      input = TrimmerDecorator.new(nameable)
+      expect(input.correct_name).to eq('Peter Pan')
     end
   end
 end
