bf5f5dea7df44bc5d9e9c2540c33113ce8ee0bdb
solly <solobarine@gmail.com>
2023-01-06 01:19:58 +0100

    Create successful test for capitalize_decorator

--- capitalize_decorator_rspec.rb
+++ capitalize_decorator_rspec.rb
@@ -1,11 +1,13 @@
+require './decorator.rb'
 require './capitalize_decorator.rb'
 
 describe CapitalizeDecorator do
   describe '#correct_name' do
     it 'Capitalize the stribg passed in it' do
-      decorate = CapitalizeDecorator.new('salty')
-      capital_string = decorate.correct_name
-      expect(capital_string).to be('SALTY')
+      nameable = double('nameable')
+      allow(nameable).to receive(:correct_name).and_return('peter')
+      decorate = CapitalizeDecorator.new(nameable)
+      expect(decorate.correct_name).to eq('Peter')
     end
   end
 end
